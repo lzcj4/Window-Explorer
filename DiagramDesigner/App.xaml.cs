ebcdce0965561ccbee07a3cecb7e5fd6a9685f2e
NICK\Nick <lzcj4@163.com>
2018-11-26 16:10:10 +0800

    1、图片控件ViewPort 和ViewBox控件VisualBrush自动填充

--- DiagramDesigner/App.xaml.cs
+++ DiagramDesigner/App.xaml.cs
@@ -12,5 +12,9 @@ namespace DiagramDesigner
     /// </summary>
     public partial class App : Application
     {
+        ///TODO: 参考资源来源
+        ///https://github.com/sachabarber/MVVMDiagramDesigner/blob/master/README.md
+        ///https://www.codeproject.com/Articles/24681/WPF-Diagram-Designer-Part-4
+        ///https://docs.microsoft.com/zh-cn/dotnet/api/system.windows.media.tilebrush.viewport?view=netframework-4.7.2#System_Windows_Media_TileBrush_Viewport
     }
 }
